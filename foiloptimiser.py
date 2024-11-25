8af59d3a0820b940f354f2f5a820d722e013c79b
Jaydosu <jaydenluu6@gmail.com>
2024-11-26 09:18:01 +1000

    UPDATED AND TRIED TO RUN SOME OPTIMISATIONS

--- foiloptimiser.py
+++ foiloptimiser.py
@@ -5,9 +5,11 @@ Pollock aerofoil: https://www.desmos.com/calculator/vmy0t1wg7x
 
 """
 # importing libraries
+import subprocess
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize, differential_evolution
 
 def create_pollock_aerofoil(thickness, chordlength, x_le, x_te, S):
   """
@@ -74,7 +76,7 @@ def create_pollock_aerofoil(thickness, chordlength, x_le, x_te, S):
   # x1 y1 (float) (float) (7 significant figures) (bottom side)
   # single line break
 
-  aerofoil_name = f'pollock_{thickness}_{chordlength}_{x_le}_{x_te}_{S}'
+  aerofoil_name = f'p_{x_le}_{x_te}_{S}'
   # lets have 50 points on the top and bottom surfaces.
   # lets make sure there are points at 0 and 1, and the bounds of piecewise functions
   # x1 = np.linspace(0, x_te * thickness, divisions)
@@ -97,10 +99,9 @@ def create_pollock_aerofoil(thickness, chordlength, x_le, x_te, S):
   iota_2 = (1 - v2) / points_div_2 # this is essentially the spacing between the points
   xbtw_2 = np.linspace(v2 + iota_2, 1, points - 4 - points_div_2)
 
-  xlist = [v1, v1 + (v2-v1)/3, v1 + 2*(v2-v1)/3, v2]
+  xlist = [v1, v1 + (v2-v1)/3, v1 + 2*(v2-v1)/3, v2] # add the bounds
   xlist = xlist + list(xbtw_1) + list(xbtw_2)
   xlist = sorted(xlist) # just in case
-  print(len(xlist))
 
   # get scaled y values for each point in xlist
   ylist = []
@@ -112,7 +113,7 @@ def create_pollock_aerofoil(thickness, chordlength, x_le, x_te, S):
     else:
       ylist.append(y3(x * chordlength) / chordlength)
 
-  with open(f'{aerofoil_name}.dat', 'w') as f:
+  with open(os.getcwd()+f'\dat\{aerofoil_name}.dat', 'w') as f:
     f.write(f'{aerofoil_name}\n\n')
     
     for x in xlist[::-1]:
@@ -127,17 +128,22 @@ def create_pollock_aerofoil(thickness, chordlength, x_le, x_te, S):
 
     return f'{aerofoil_name}.dat'
 
-thickness, chordlength, x_le, x_te, S = 20, 235, 2, 2, 0.25
-aerofoil = create_pollock_aerofoil(thickness, chordlength, x_le, x_te, S)
-
-example_aerofoil = 'pollock_20_235_2_2_0.25.dat'
-
-# Run xfoil with the generated aerofoil dat file
 def run_xfoil(aerofoil_dat):
-  
+  """
+  Function to run xfoil with the generated aerofoil dat file. Returns the cl_max and aoa_max of the aerofoil.
+
+  Parameters:
+  aerofoil_dat: str
+    Dat file of the aerofoil.
+  """
+  # ensure that there is no polar file for the aerofoil
+  if os.path.exists(os.getcwd() + '\pol\\' + aerofoil_dat.replace('.dat', '.pol')):
+    os.remove(os.getcwd() + '\pol\\' + aerofoil_dat.replace('.dat', '.pol'))
+
   xfoilpath = 'xfoil.exe'
   commands = f"""
-  LOAD {aerofoil_dat}
+  LOAD dat\{aerofoil_dat}
+
   MDES
   FILT
   EXEC
@@ -152,11 +158,14 @@ def run_xfoil(aerofoil_dat):
   
   OPER
   ITER 200
-  VISC 1000000
-  PACC
-  {aerofoil_dat.replace('.dat', '.pol')}
+  VPAR
+  N 13
   
-  ASEQ 0 5 0.1
+  VISC 1439701
+  PACC
+  pol\{aerofoil_dat.replace('.dat', '.pol')}
+  
+  ASEQ 0 6 0.1
   PACC
   QUIT
   """
@@ -164,8 +173,95 @@ def run_xfoil(aerofoil_dat):
   with open('xfoil_input.txt', 'w') as f:
     f.write(commands)
   
-  os.system(f'{xfoilpath} < xfoil_input.txt')
+  with open(os.devnull, 'w') as devnull:
+    # Set up the startup info to prevent a console window from appearing
+    startupinfo = subprocess.STARTUPINFO()
+    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    startupinfo.wShowWindow = subprocess.SW_HIDE
+    CREATE_NO_WINDOW = 0x08000000
 
-# Run xfoil with the generated aerofoil dat file
-run_xfoil(example_aerofoil)
+    subprocess.run([xfoilpath], shell=True, stdin=open('xfoil_input.txt'), stdout=devnull, stderr=devnull, startupinfo=startupinfo, creationflags=subprocess.CREATE_NO_WINDOW)
 
+def read_pol(aerofoil_pol):
+  with open(os.getcwd() + '\pol\\' + aerofoil_pol, 'r') as f:
+    lines = f.readlines()
+    
+    # find max cl
+    cl_max = 0
+    aoa_max = 0
+    for line in lines[13:]:
+      cl = float(line.split()[1])
+      aoa = float(line.split()[0])
+      if cl > cl_max:
+        cl_max = cl
+        aoa_max = aoa
+
+  return cl_max, aoa_max
+
+def objective_function(param):
+  # Clean up params to remove floating point errors
+  param = [round(p, 3) for p in param]
+
+  # Generate the aerofoil data file for the current parameter
+  aerofoil_dat = create_pollock_aerofoil(thickness, chordlength, x_le, param[0], param[1])
+
+  print("Current parameters: ", param)
+  print("Current aerofoil: ", aerofoil_dat)
+  
+  # Run xfoil with the generated aerofoil dat file
+  run_xfoil(aerofoil_dat)
+  
+  # Read the results
+  cl_max, aoa_max = read_pol(aerofoil_dat.replace('.dat', '.pol'))
+
+  cl_max = float(cl_max)
+  
+  # We want to maximize cl_max, so we return its negative for minimization
+  print("Current cl_max: ", cl_max)
+  return -cl_max, aoa_max, aerofoil_dat
+
+
+# Baseline parameters
+# immutable parameters
+thickness = 22
+chordlength = 235
+x_le = 2.468
+
+baseline = [2.143, 0.803]
+
+# Define the bounds for the parameters
+bounds = [(2, 2.3), (.78, 0.85)] # x_le, x_te, S
+
+# Perform the optimization
+result = minimize(lambda param: objective_function(param)[0], baseline, bounds=bounds, method='L-BFGS-B', options={'ftol': 1e-9, 'eps': 1e-3, 'maxiter':100})
+#result = differential_evolution(lambda param: objective_function(param)[0], bounds, maxiter=10, popsize=10, disp=True)
+
+# Extract the optimal parameters
+optimal_params = result.x
+optimal_aerofoil_dat = objective_function(optimal_params)[2]
+optimal_cl_max = objective_function(optimal_params)[0]
+optimal_aoa_max = objective_function(optimal_params)[1]
+
+list_of_x_te = []
+list_of_S = []
+list_of_cl = []
+
+for pol in os.listdir(os.getcwd() + '\pol'):
+  list_of_x_te.append(float(pol.split('_')[2]))
+  list_of_S.append(float(pol.split('_')[3].split('.')[0]))
+  list_of_cl.append(read_pol(pol)[0])
+
+# Plot the results and colour by cl
+plt.scatter(list_of_x_te, list_of_S, c=list_of_cl, cmap='viridis')
+plt.colorbar()
+plt.xlabel('x_te')
+plt.ylabel('S')
+plt.title('Optimisation of Ozgoose Foil')
+plt.show()
+
+'''
+ozgoose = [4, 6, 0.75]
+ozgoose_dat = create_pollock_aerofoil(thickness, chordlength, ozgoose[0], ozgoose[1], ozgoose[2])
+run_xfoil(ozgoose_dat)
+ozgoose_cl_max, ozgoose_aoa_max = read_pol(ozgoose_dat.replace('.dat', '.pol'))
+print(f'Ozgoose cl_max: {ozgoose_cl_max}')'''
